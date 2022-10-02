a2aa2b7e021e71ae08c798f6d6ef6068fed9d1cf
larissa-abboud <lariabboud@gmail.com>
2022-10-02 22:57:12 +0300

    game functions added

--- js_ttt.js
+++ js_ttt.js
@@ -1,3 +1,25 @@
 window.onload=() => {
-    
+    /**
+     * onclick adds an img
+     * flag to show which player
+     * even= x
+     * odd =o
+     * loop of 9 
+     * 
+     */
+     let game_status = ["", "", "", "", "", "", "", "", ""];
+     let currentP_layer = "X";
+     // wins if 
+     /**
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+      */
+     
+     
 }
